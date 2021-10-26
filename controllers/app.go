7db3a3fba8d10215c6a9926fbd4be27ca062cd7f
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-26 16:12:36 +0100

    All Handlers needed for the Oauth Added

--- controllers/app.go
+++ controllers/app.go
@@ -1,4 +1,30 @@
 package controllers
 
+import (
+	"fmt"
+	"github.com/markbates/goth/gothic"
+	"html/template"
+	"log"
+	"net/http"
+)
 
+func BeginAuth(rw http.ResponseWriter, req *http.Request) {
+	gothic.BeginAuthHandler(rw, req)
+}
 
+func LoginPage(rw http.ResponseWriter, req *http.Request) {
+	t, _ := template.ParseFiles("./templates/login.gohtml")
+	t.Execute(rw, nil)
+
+}
+
+func Callback(rw http.ResponseWriter, req *http.Request) {
+	user, err := gothic.CompleteUserAuth(rw, req)
+	if err != nil {
+		fmt.Fprintf(rw, "Error : %v", err.Error())
+		return
+	}
+	log.Println(user)
+	t, _ := template.ParseFiles("./templates/profile.gohtml")
+	t.Execute(rw, user)
+}

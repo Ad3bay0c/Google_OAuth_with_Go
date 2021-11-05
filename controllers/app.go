4a27ceb12e4febda0c03a41fa6401a4fb601fbb4
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-05 21:39:33 +0100

    Oauth handlers

--- controllers/app.go
+++ controllers/app.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"github.com/markbates/goth/gothic"
+	"github.com/markbates/goth/providers/google"
+	"golang.org/x/oauth2"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 func BeginAuth(rw http.ResponseWriter, req *http.Request) {
@@ -27,4 +33,30 @@ func Callback(rw http.ResponseWriter, req *http.Request) {
 	log.Println(user)
 	t, _ := template.ParseFiles("./templates/profile.gohtml")
 	t.Execute(rw, user)
+}
+
+var googleOAuthConfig = &oauth2.Config{
+	RedirectURL: "http://localhost:3500/auth/google/callback",
+	ClientID: os.Getenv("GOOGLE_OAUTH_CLIENT_ID"),
+	ClientSecret: os.Getenv("GOOGLE_OAUTH_CLIENT_SECRET"),
+	Scopes: []string{"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint: google.Endpoint,
+}
+
+func OAuthLogin(w http.ResponseWriter, req *http.Request) {
+	// Create Oauth State Cookie
+	oauthState := generateStateOauthCookie(w)
+	u := googleOAuthConfig.AuthCodeURL(oauthState)
+	http.Redirect(w, req, u, http.StatusTemporaryRedirect)
+}
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	var expiration = time.Now().Add(365 * 24 * time.Hour)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	http.SetCookie(w, &cookie)
+
+	return state
 }

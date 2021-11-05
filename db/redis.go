ee15aa3916df8e8a4a1d11b128078224461bd610
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-05 21:39:58 +0100

    redis databse implemented

--- db/redis.go
+++ db/redis.go
@@ -1,1 +1,18 @@
-package models
+package db
+
+import (
+	"github.com/go-redis/redis"
+	"log"
+)
+
+func RedisConnect() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+
+	pong, _ := client.Ping().Result()
+	log.Println(pong)
+	return client
+}

cb36447b8fff8443b3a0e2feb2c05694d824eab8
Ad3bay0c <aclintonadebayo@gmail.com>
2021-11-06 10:56:25 +0100

    Redis Connection Intialozed

--- db/redis.go
+++ db/redis.go
@@ -1,15 +1,16 @@
 package db
 
 import (
-	"github.com/go-redis/redis"
 	"log"
+
+	"github.com/go-redis/redis"
 )
 
 func RedisConnect() *redis.Client {
 	client := redis.NewClient(&redis.Options{
-		Addr: "localhost:6379",
+		Addr:     "localhost:6379",
 		Password: "",
-		DB: 0,
+		DB:       0,
 	})
 
 	pong, _ := client.Ping().Result()
